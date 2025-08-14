4003e1ad2767629933933e37512f45cc5342a2f9
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-08-14 22:23:10 +0300

    Update CorsConfig.java

--- src/main/java/com/example/streammateseriessvc/app/configs/CorsConfig.java
+++ src/main/java/com/example/streammateseriessvc/app/configs/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Позволява всички пътища
-                .allowedOrigins("https://glad-candida-lumo-user-svc-ee901669.koyeb.app", "http://localhost:8080")  // Разрешава само даден домейн
+                .allowedOrigins("https://lumo-api-gateway.onrender.com", "http://localhost:8080")  // Разрешава само даден домейн
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Разрешава само определени методи
                 .allowedHeaders("*");  // Позволява всички хедъри
     }
